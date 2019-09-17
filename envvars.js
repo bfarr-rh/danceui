4fc5fd530743a8a0625d28bc14623ef293a5b3d6
bfarr-rh <bfarr@redhat.com>
2019-09-17 17:57:14 +0900

    adjust urls

--- envvars.js
+++ envvars.js
@@ -1,5 +1,5 @@
-var URL_TRACKING_DEFAULT = "http://mlflow-audio-mlflow.apps.cluster-gartner-416c.gartner-416c.openshiftworkshop.com";
-var URL_DANCEAPI_DEFAULT = "http://dance-api-robot-dance.apps.cluster-gartner-416c.gartner-416c.openshiftworkshop.com";
+var URL_TRACKING_DEFAULT = "http://mlflow-audio-mlflow.apps.cluster-melba-80af.melba-80af.open.redhat.com";
+var URL_DANCEAPI_DEFAULT = "http://dance-api-robot-dance.apps.cluster-melba-80af.melba-80af.open.redhat.com";
 //var URL_TRACKING = {{URL_TRACKING}};
 //var URL_DANCEAPI = {{URL_DANCEAPI}};
 
